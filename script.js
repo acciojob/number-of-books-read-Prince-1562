61403d3cf28a880059aaa52a3126a3592bb452d5
Prince-1562 <princemakani1562@gmail.com>
2025-05-15 07:48:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,9 @@
 const library = [
-  {
-    author: "Bill Gates",
-    title: "The Road Ahead",
-    readingStatus: true,
-  },
-  {
-    author: "Steve Jobs",
-    title: "Walter Isaacson",
-    readingStatus: true,
-  },
-  {
-    author: "Suzanne Collins",
-    title: "Mockingjay: The Final Book of The Hunger Games",
-    readingStatus: false,
-  },
+  { title: "The Road Ahead", author: "Bill Gates", readingStatus: true },
+  { title: "Walter Isaacson", author: "Steve Jobs", readingStatus: true },
+  { title: "Mockingjay", author: "Suzanne Collins", readingStatus: false }
 ];
 
-const numberOfBooksRead = () => {
-  // write your code here
-};
-
-// Do not change the code below
+function numberOfBooksRead() {
+  return library.filter(book => book.readingStatus).length;
+}
